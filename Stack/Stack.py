aed40ddfecfa7f645ddf7e956909aefc46e5b274
Nikul Patel <patel.nikul03@gmail.com>
2019-04-10 22:52:18 -0400

    Added balanced parenthesis methods

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -35,15 +35,70 @@ print(s.pop())
 print(s.pop())
 print(s.size())
 
-
+# Reverse String function
 def revstring(mystr):
     s = Stack()
-    newList = []
-    input = list(mystr)
-    for x in input:
-        s.push(x)
+    for ch in mystr:
+        s.push(ch)
+    revstr = ""
     while not s.isEmpty():
-        newList.append(s.pop())
-    return ''.join(newList)
+        revstr = revstr + s.pop()
+    return revstr
 
 print(revstring("hello"))
+
+# Simple balance paranthses program
+def parChecker(symbolStr):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolStr) and balanced:
+        ch = symbolStr[index]
+        if ch == "(":
+            s.push(ch)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+        index = index + 1
+
+    if s.isEmpty() and balanced:
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
+
+#General case of parantheses check
+
+def generalParChecker(symbolStr):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolStr) and balanced:
+        ch = symbolStr[index]
+        if ch in "({[":
+            s.push(ch)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, ch):
+                    balanced = False
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(top, ch):
+    opening = "({["
+    closing = ")}]"
+    return opening.index(top) == closing.index(ch)
+
+print(generalParChecker('{{([][])}()}'))
+print(generalParChecker('[{()]'))
