2d27a69f7703b696bb89c2acc23581d8fa31d7c2
Nikul Patel <patel.nikul03@gmail.com>
2019-04-29 23:55:42 -0400

    changeXY prg

--- Recursion/changeXY.py
+++ Recursion/changeXY.py
@@ -7,3 +7,18 @@ changeXY("xxhixx") → "yyhiyy"
 changeXY("xhixhix") → "yhiyhiy"
 
 """
+
+def changeXY(s):
+    res = ""
+    if len(s) < 1:
+        return s
+    else:
+        ch = s[0]
+        if ch == 'x':
+            return 'y' + changeXY(s[1:])
+        else:
+            return ch + changeXY(s[1:])
+
+print(changeXY("codex"))
+print(changeXY("xxhixx"))
+print(changeXY("xhixhix"))
