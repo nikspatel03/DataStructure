3f59b680d967134cfb17a6ae887ec4b08e30dc47
Nikul Patel <patel.nikul03@gmail.com>
2019-05-07 19:01:36 -0400

    Implemented binarySearch algorithm

--- Searching/binarySearch.py
+++ Searching/binarySearch.py
@@ -4,7 +4,8 @@ Implementation of recursive binary search algorithm
 
 """
 
-
+"""
+## This is still O(n) because spliting the list takes O(n) runtime
 def binarySearch(arr, item):
     if len(arr) == 0:
         return False
@@ -18,6 +19,23 @@ def binarySearch(arr, item):
                 return binarySearch(arr[:mid], item)
             else:
                 return binarySearch(arr[mid+1:], item)
+"""
+## O(log n) version  using the index approach
+def binarySearch(alist, item):
+    return binarySearcHelper(alist, item, 0, len(alist) - 1)
+
+def binarySearcHelper(alist, item, low, high):
+    if low > high:
+        return False
+    else:
+        mid = low + (high - low) // 2
+
+        if alist[mid] == item:
+            return True
+        elif alist[mid] > item:
+            return binarySearcHelper(alist, item, low, mid - 1)
+        else:
+            return binarySearcHelper(alist, item, mid + 1, high)
 
 
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
